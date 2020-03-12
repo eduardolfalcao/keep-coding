38ab59193a9f96bf9de95416796e8ddfb2c1f4c2
Eduardo Falcão <eduardolfalcao@gmail.com>
2020-03-12 00:14:08 -0300

    Added HelloWorld and compiling instructions

---
+++ cpp/content/CompileRun.md
@@ -0,0 +1,22 @@
+# Compile and run on CPP
+
+## Compile
+
+g++ Program.cpp -o Program.out
+
+## Run
+
+./Program.out
+
+## HelloWorld example
+
+```cpp
+#include <iostream>
+
+using namespace std; 
+
+int main(){
+	cout<<"Hello world... keep coding :)\n";
+	cin.get(); 
+}
+```
---
+++ cpp/source/HelloWorld.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+// cout function requires iostream
+
+using namespace std;
+// use a group of functions that are part of the standard library (std)
+
+int main(){
+	cout<<"Hello world... keep coding :)\n";
+	cin.get();
+
+	return 1;	//execution succeeded
+}
