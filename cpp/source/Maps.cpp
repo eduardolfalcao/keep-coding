6e731998dc65bd21e5412dc8fe35573a43aba2e8
Eduardo Falcão <eduardolfalcao@gmail.com>
2020-03-25 09:37:56 -0300

    Finished example source code with maps

--- cpp/source/Maps.cpp
+++ cpp/source/Maps.cpp
@@ -1,9 +1,21 @@
 #include <map>
 #include <unordered_map>
 #include <iostream>
-#include <cassert>
 
 using namespace std;
+
+void printMap(map<string, int> myMap){
+	//for each
+	for (auto& pair: myMap) 
+		cout << pair.first << ": " << pair.second << endl;
+}
+
+void printMap(unordered_map<string, int> myMap){
+	unordered_map<string,int>::iterator it;
+	//iterator
+	for (it = myMap.begin(); it != myMap.end(); it++) 
+  		cout << it->first << ": " << it->second << endl; 
+}
 
 int main(int argc, char **argv){
 	//string is the key, int is the value
@@ -17,60 +29,85 @@ int main(int argc, char **argv){
 	//find: moves iterator to element
 	//if element is not present, then iterator goes to end
 	if(coronaVirusCases.find("china") != coronaVirusCases.end())
-		cout << coronaVirusCases["china"] << endl;
+		cout << "Corona virus cases on China: " << coronaVirusCases["china"] << endl;
 
-	//count elements with specific key
-	cout << coronaVirusCases.count("china") << endl;
+	//count: returns 1 if key is present, 0 otherwise
+	cout << "Is china key present on map? " << coronaVirusCases.count("china") << endl << endl;
 
-	//insert: adds the pair and the sort map w.r.t key (if ordered map)
+	//insert: adds the pair and then sort map w.r.t key (if ordered map)
 	//if the key exists, then nothing is inserted
 	coronaVirusCases.insert(pair<string,int>("brazil", 2247));
+	cout << "Inserting brazil via insert with 2247 cases, on map" << endl;
 	if(coronaVirusCases.find("brazil") != coronaVirusCases.end())
-		cout << coronaVirusCases["brazil"] << endl;
+		cout << "Corona virus cases on Brazil: " << coronaVirusCases["brazil"] << endl << endl;
+
 	coronaVirusCases.insert(pair<string,int>("brazil", 3000));
+	cout << "Inserting brazil via insert  with 3000 cases, on map" << endl;
 	if(coronaVirusCases.find("brazil") != coronaVirusCases.end())
-		cout << coronaVirusCases["brazil"] << endl;
+		cout << "Corona virus cases on Brazil: " << coronaVirusCases["brazil"] << endl << endl;
 	
 	//emplace: the same as insert, but pair is constructed in-place function
 	//if the key exists, then nothing is inserted
 	coronaVirusCases.emplace("brazil",4000);
+	cout << "Inserting brazil via emplace with 4000 cases, on map" << endl;
 	if(coronaVirusCases.find("brazil") != coronaVirusCases.end())
-		cout << coronaVirusCases["brazil"] << endl;
+		cout << "Corona virus cases on Brazil: " << coronaVirusCases["brazil"] << endl << endl;
 
 	//assignment: replaces the value
 	coronaVirusCases["brazil"] = 5000;
+	cout << "Inserting brazil via assignment with 5000 cases, on map" << endl;
 	if(coronaVirusCases.find("brazil") != coronaVirusCases.end())
-		cout << "####" << coronaVirusCases["brazil"] << endl;
-	cout << "Brazil count: " << coronaVirusCases.count("brazil") << endl;
+		cout << "Corona virus cases on Brazil: " << coronaVirusCases["brazil"] << endl << endl;
+	
+	cout << "Is brazil key present on map? " << coronaVirusCases.count("brazil") << endl << endl;
+
 	//at: replaces the value
 	coronaVirusCases.at("brazil") = 6000;
+	cout << "Inserting brazil via at with 6000 cases, on map" << endl;
 	if(coronaVirusCases.find("brazil") != coronaVirusCases.end())
-		cout << "####" << coronaVirusCases["brazil"] << endl;
-	cout << "Brazil count: " << coronaVirusCases.count("brazil") << endl;
-	
-	
+		cout << "Corona virus cases on Brazil: " << coronaVirusCases["brazil"] << endl << endl;
+
+
 	map<string,int>::iterator it;
 	it = coronaVirusCases.find("brazil");
 	//erase pair where iterator is pointing
-	coronaVirusCases.erase (it);
-	cout << "Brazil count after erasing:" << coronaVirusCases.count("brazil") << endl;
+	coronaVirusCases.erase(it);
+	cout << "Is brazil key present on map after erasing: " << coronaVirusCases.count("brazil") << endl << endl;
 
-	cout << "Map max size:" << coronaVirusCases.max_size() << endl;
-	cout << "Map size:" << coronaVirusCases.size() << endl;
+	cout << "Map max size: " << coronaVirusCases.max_size() << endl;
+	cout << "Map size: " << coronaVirusCases.size() << endl;
 	cout << "Map is empty? " << coronaVirusCases.empty() << endl << endl;
 
-	cout << "## Iterating over maps ##" << endl;
+	cout << "## Iterating over map with for each ##" << endl;
 	//for each
 	for (auto& pair: coronaVirusCases) 
-		cout << pair.first << ": " << pair.second << endl;
-		      
+		cout << pair.first << ": " << pair.second << endl;	      
 	cout << endl;
 
+	cout << "## Iterating over map with iterator ##" << endl;
 	//iterator
 	for (it = coronaVirusCases.begin(); it != coronaVirusCases.end(); it++) 
   		cout << it->first << ": " << it->second << endl; 
-
 	cout << endl;
+
+	map<string, int> coronaVirusCasesDoubled;
+	coronaVirusCasesDoubled["china"] = 162342;
+	coronaVirusCasesDoubled["italy"] = 138352;
+	coronaVirusCasesDoubled["usa"] = 107218;
+	coronaVirusCasesDoubled["spain"] = 84116;
+	coronaVirusCasesDoubled["germany"] = 65982;
+
+	//swap: swap contents of two maps
+	cout << "Swapping contents of coronaVirusCases with coronaVirusCasesDoubled" << endl;
+	coronaVirusCases.swap(coronaVirusCasesDoubled);
+	cout << "Corona Virus Cases variable:" << endl;
+	printMap(coronaVirusCases);
+	cout << endl;
+        cout << "Corona Virus Cases Doubled variable:" << endl;
+	printMap(coronaVirusCasesDoubled);
+	cout << endl << endl;
+	//swapping back
+	coronaVirusCases.swap(coronaVirusCasesDoubled);
 
 	unordered_map<string, int> coronaVirusDeaths;
 	coronaVirusDeaths["china"] = 3277;
@@ -78,16 +115,23 @@ int main(int argc, char **argv){
 	coronaVirusDeaths["usa"] = 698;
 	coronaVirusDeaths["spain"] = 2991;
 	coronaVirusDeaths["germany"] = 159;
-	
-	//for each
-	for (auto& pair: coronaVirusDeaths) 
-		cout << pair.first << ": " << pair.second << endl;
 
-	cout << "Map max size:" << coronaVirusDeaths.max_size() << endl;
-	cout << "Map size:" << coronaVirusDeaths.size() << endl;
-	cout << "Map is empty? " << coronaVirusDeaths.empty() << endl << endl;
-	//swap
+	cout << "Print unordered Map: "	<< endl;
+	printMap(coronaVirusDeaths);
+	cout << endl;
+
+	unordered_map<string, int> coronaVirusNotDead;
+	
+	for (auto& pair: coronaVirusCases) 
+		coronaVirusNotDead[pair.first] = pair.second - coronaVirusDeaths[pair.first];
+	
+	cout << "Corona virus not dead: (cases - deaths) "  << endl;
+	printMap(coronaVirusNotDead);
+	cout << endl;
+
 	//clear
+	coronaVirusNotDead.clear();
+	cout << "Size of map after clear(): " << coronaVirusNotDead.size() << endl;
 	
 	return 0;
 }
